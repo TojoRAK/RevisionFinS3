361620da729e3f2dd8bf13902102c2da7bffac08
TojoRAK <tojomarofitahiana@gmail.com>
2026-02-10 11:43:19 +0300

    [fix] ajout colonne email users

---
+++ app/controllers/AuthClient.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\CategorieModel;
+use Flight;
+use flight\Engine;
+
+class CategorieController
+{
+    protected Engine $app;
+
+    public function __construct($app)
+    {
+        $this->app = $app;
+    }
+
+    public function doLogin(){
+        $name = 
+
+
+
+    }
+  
+   
+}
