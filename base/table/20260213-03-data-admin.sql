3450e5e40cfa09e558a964d0c8d951b78c50b4fe
RAKOTOMILIARISON Lucas <lucasrakotomily@gmail.com>
2026-02-13 09:05:28 +0300

    dataAdmin

---
+++ base/table/20260213-03-data-admin.sql
@@ -0,0 +1,3 @@
+--Admin mot de passe : admin1234
+INSERT INTO users (email, name, role, password_hash) VALUES
+('admin@takalo.tld','Admin', 'ADMIN', '$2y$12$wXj6MQUbuDFDU4B2/H6mHe7QHKm4mqQmYnSFaDwEUgRI8UhgY0Hr6');
